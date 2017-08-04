50a4066e9bee320e5cea5f8e1ac0e4badfd2be9d
digitalbricklayer <jhughes@openxtra.co.uk>
2017-08-04 10:32:36 +0100

    Fixes message in run-dev-server.sh.

--- run-dev-server.sh
+++ run-dev-server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo Starting Jekyll Development Web Server...
+echo 'Starting Jekyll Development Web Server...'
 bundle exec jekyll serve -w --incremental --config _config.yml,_config-dev.yml
 
